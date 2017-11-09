3ba7f2f77824be56cbe6e8171f14953c6ea82ce1
jmshc <1204488658@qq.com>
2017-11-09 20:03:12 +0800

    Site updated: 2017-11-09 20:03:12

--- js/bofang.js
+++ js/bofang.js
@@ -1,4 +1,4 @@
-    window.onload = function () {
+   /* window.onload = function () {
       var obj = document.getElementById("selectId");
       var url = document.getElementById("addressUrl");
       var videoObj = document.getElementById("player");
@@ -7,4 +7,25 @@
           videoObj.src = obj.value + url.value
         }
       }
+    }   1.1 版本的 js*/
+	
+	
+window.onload = function () {
+      var obj = document.getElementById("selectId");
+      var url = document.getElementById("addressUrl");
+      var videoObj = document.getElementById("player");
+	  var a=['top','left'],b=0; 
+      document.getElementById('played').onclick = function () {
+        if (obj.value && url.value) {
+		 videoObj.src = obj.value + url.value;
+
+        }else
+		{
+        var u=setInterval(function(){ 
+         url.style[a[b%2]]=(b++)%4<2?0:4; 
+         if(b>25){clearInterval(u);b=0} 
+         },15) 
+		}
+      }
     }
+	// 1.2 版本js 20171109 by xies
