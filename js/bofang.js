fe72c00d9b5278c514983a533183dede5e570c5f
jmshc <1204488658@qq.com>
2018-02-01 12:51:44 +0800

    Site updated: 2018-02-01 12:51:44

--- js/bofang.js
+++ js/bofang.js
@@ -16,6 +16,7 @@ window.onload = function () {
       var videoObj = document.getElementById("player");
 	  var a=['top','left'],b=0; 
       document.getElementById('played').onclick = function () {
+        play();
         if (obj.value && url.value) {
 		 videoObj.src = obj.value + url.value;
 
@@ -27,5 +28,47 @@ window.onload = function () {
          },30) 
 		}
       }
+      
+      changImg();
     }
+  function changImg(){
+    var changeText= Math.random()*100000+'';
+    if(document.getElementById('v-bg')){
+      
+    document.getElementById('v-bg').innerHTML=changeText.substring(0,4);
+
+    }
+  }
+
+  function changeRegister(){
+    document.getElementById('register').style.display="block";
+     document.getElementById('login').style.display="none";
+  }
+function play () {
+   var addressUrl=$("#addressUrl").val();
+   if(addressUrl=="")
+   {
+    $("#players").removeClass('shake_effect');  
+    setTimeout(function()
+    {
+     $("#players").addClass('shake_effect')
+    },1); 
+    return false;   
+   }
+  }
+  function login(LoginForm)
+{
+  if (LoginForm.username.value == "")
+  {
+    alert("请输入用户名!");
+    LoginForm.username.focus();
+    return (false);
+  }
+  if (LoginForm.password.value == "")
+  {
+    alert("请输入密码!");
+    LoginForm.password.focus();
+    return (false);
+  }
+}
 	// 1.2 版本js 20171109 by xies
